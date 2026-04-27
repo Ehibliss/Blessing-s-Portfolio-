2ceec7ab4f57a7a8b8d57fad7affe77606369e4c
Ehibliss <ochemeblessingehi@gmail.com>
2026-04-27 12:54:42 +0100

    update colors

--- src/Components/Headercomponents/Header.jsx
+++ src/Components/Headercomponents/Header.jsx
@@ -1,32 +1,53 @@
-import React from "react";
+import { useState } from "react";
 import "./Header.css";
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       <header>
-        <div className="logo">
-          <img src="/logo.png" alt="brand logo" className="bliss-log" />
-        </div>
+        <div className="header-container">
+          <div className="logo">
+            <img src="/logo.png" alt="brand logo" className="bliss-log" />
+          </div>
 
-        <nav>
-          <ul className="navbar-menu">
-            <li>
-              <a href="#Home-page">Home</a>
-            </li>
-            <li>
-              <a href="#About-section">About</a>
-            </li>
-            <li>
-              <a href="#Project-section">Projects</a>
-            </li>
-            <li>
-              <a href="#skill-section">Skils</a>
-            </li>
-            <li>
-              <a href="#Contact-section">Contact</a>
-            </li>
-          </ul>
-        </nav>
+          <div
+            className={`hamburger ${menuOpen ? "active" : ""}`}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <nav className={menuOpen ? "nav active" : "nav"}>
+            <ul className="navbar-menu">
+              <li>
+                <a href="#Home-page" onClick={() => setMenuOpen(false)}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#About-section" onClick={() => setMenuOpen(false)}>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#Project-section" onClick={() => setMenuOpen(false)}>
+                  Projects
+                </a>
+              </li>
+              <li>
+                <a href="#skill-section" onClick={() => setMenuOpen(false)}>
+                  Skills
+                </a>
+              </li>
+              <li>
+                <a href="#Contact-section" onClick={() => setMenuOpen(false)}>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     </>
   );
