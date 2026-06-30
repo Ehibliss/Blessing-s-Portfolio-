201b74594a0551cd9fd469b91f198ac26b00c280
Ehibliss <ochemeblessingehi@gmail.com>
2026-07-01 00:42:08 +0100

    worked on header

--- src/Components/Headercomponents/Header.jsx
+++ src/Components/Headercomponents/Header.jsx
@@ -21,27 +21,27 @@ function Header() {
           </div>
           <nav className={menuOpen ? "nav active" : "nav"}>
             <ul className="navbar-menu">
-              <li>
+              <li className="nav-link">
                 <a href="#Home-page" onClick={() => setMenuOpen(false)}>
                   Home
                 </a>
               </li>
-              <li>
+              <li className="nav-link">
                 <a href="#About-section" onClick={() => setMenuOpen(false)}>
                   About
                 </a>
               </li>
-              <li>
+              <li className="nav-link">
                 <a href="#Project-section" onClick={() => setMenuOpen(false)}>
                   Projects
                 </a>
               </li>
-              <li>
+              <li className="nav-link">
                 <a href="#skill-section" onClick={() => setMenuOpen(false)}>
                   Skills
                 </a>
               </li>
-              <li>
+              <li className="nav-link">
                 <a href="#Contact-section" onClick={() => setMenuOpen(false)}>
                   Contact
                 </a>
